fe94a3e3a03d23e6d49b53ede512a1157d1a39df
Svitlana Savikhina <savihina111@gmail.com>
2023-10-20 19:23:10 +0300

    Register all models

--- social_network/admin.py
+++ social_network/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from social_network.models import (
+    Post,
+    Like,
+    Comment,
+    ScheduledPost
+)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "user", "created_at")
+    search_fields = ("title",)
+    list_filter = ("user",)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user", "post", "created_at")
+    search_fields = ("user",)
+    list_filter = ("post",)
+
+
+@admin.register(ScheduledPost)
+class ScheduledPostAdmin(admin.ModelAdmin):
+    list_display = ("title", "user", "created_at")
+    search_fields = ("title",)
+    list_filter = ("user",)
+
+
+admin.site.register(Like)
