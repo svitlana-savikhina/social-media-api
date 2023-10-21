3c260d46a7cfcd4b35622dc456376977ad9ef9c4
Svitlana Savikhina <savihina111@gmail.com>
2023-10-21 17:07:20 +0300

    Create serializers for all models

---
+++ user/serializers.py
@@ -0,0 +1,40 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from user.models import Profile, UserFollowing
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "email", "password", "is_staff")
+        read_only = ("id", "is_staff")
+        extra_kwargs = {"password": {"write_only": True}, "min_length": 5}
+
+    def create(self, validated_data):
+        return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ("user", "bio", "profile_picture")
+
+
+class ProfileImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ("id", "image")
+
+
+class UserFollowingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserFollowing
+        fields = ("user", "followed_user")
